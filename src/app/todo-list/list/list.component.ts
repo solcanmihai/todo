1531c10a696b94cc554e05923b880747bd123757
Solcan Mihai <solcan.mihaiandrei@yahoo.com>
2017-07-17 17:38:12 +0300

    fixed more little bugs

--- src/app/todo-list/list/list.component.ts
+++ src/app/todo-list/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import {TodoItem} from './../../todo-item';
+import {TodoItem} from './../todo-item';
 
 import {TodoService} from '../todo.service';
 
